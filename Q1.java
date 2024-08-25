8878a80afb78477311b6f3f3f2020ee345424f54
S.M Mahmudur Rahman SIfat <mahmudurrahmansifat6@gmail.com>
2024-08-26 02:21:47 +0600

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Q1{
+    public static void main(String args[]){
+        Scanner src=new Scanner(System.in);
+        System.out.println("Give : ");
+        String name=src.nextLine();
+        int count=name.length();
+        System.out.println("Length is : "+count);
+        
+    }
+}
---
+++ Q2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Q2 {
+    public static void main(String args[ ]){
+        Scanner src=new Scanner(System.in);
+        String a1=src.nextLine();
+        String a2=src.nextLine();
+        String s=a1 +a2;
+        System.out.println(s);
+    }
+    
+}
